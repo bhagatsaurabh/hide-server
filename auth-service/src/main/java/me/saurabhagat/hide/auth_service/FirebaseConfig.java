b2f651f7158af0c2d3e53180f6667850cdfa47fb
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-02-20 22:37:09 +0530

    scaffold: monorepo

---
+++ auth-service/src/main/java/me/saurabhagat/hide/auth_service/FirebaseConfig.java
@@ -0,0 +1,43 @@
+package me.saurabhagat.hide.auth_service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Objects;
+
+@Configuration
+public class FirebaseConfig {
+    @Value("${firebase.admin.key:}")
+    private Resource firebaseKeyPath;
+
+    @Value("${firebase.admin.key.base64:}")
+    private String firebaseKeyBase64;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FirebaseOptions options;
+        if (Objects.nonNull(firebaseKeyPath) && firebaseKeyPath.exists()) {
+            try (var inputStream = firebaseKeyPath.getInputStream()) {
+                options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(inputStream))
+                        .build();
+            }
+        } else if (Objects.nonNull(firebaseKeyBase64) && !firebaseKeyBase64.isEmpty()) {
+            byte[] decodedBytes = Base64.getDecoder().decode(firebaseKeyBase64);
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(decodedBytes)))
+                    .build();
+        } else {
+            throw new RuntimeException("Firebase credentials not found");
+        }
+        return FirebaseApp.initializeApp(options);
+    }
+}
