6af15e522e5b438cac051bb0807419badad333da
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-03-13 22:35:28 +0530

    scaffold: postgres, workspace service, redis

--- user/src/search/search.module.ts
+++ user/src/search/search.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
+import { TypesenseService } from 'src/typesense/typesense.service';
 
 @Module({
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, TypesenseService],
 })
 export class SearchModule {}
