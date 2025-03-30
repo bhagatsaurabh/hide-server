7ed706e357a24a00b8358cbfd2c8e4c5e30ac548
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-03-30 21:40:51 +0530

    bugfixes

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     },
   });
   await app.startAllMicroservices();
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 80);
 }
 bootstrap();
