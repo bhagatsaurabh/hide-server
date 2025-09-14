d5da027ea99c7980747dcc8ccec12de5193b29e2
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-09-14 22:51:49 +0530

    scaffold: k8s configs

--- workspace/src/app.module.ts
+++ workspace/src/app.module.ts
@@ -16,7 +16,7 @@ import { RedisModule } from 'hide-redis';
       host: process.env.POSTGRES_HOST!,
       port: parseInt(process.env.POSTGRES_PORT!),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASS,
+      password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       entities: [Workspace, Membership],
       synchronize: process.env.NODE_ENV === 'development',
