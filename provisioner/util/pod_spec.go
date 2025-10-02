b968021ba88f0db39914f649c16379d74b0a0db8
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2025-10-02 16:49:11 +0530

    bugfixes

--- provisioner/util/pod_spec.go
+++ provisioner/util/pod_spec.go
@@ -80,11 +80,11 @@ func GetPodSpec(wsUuid string, image string, publicKey string, wsType string) *v
 					Resources: v1.ResourceRequirements{
 						Requests: v1.ResourceList{
 							v1.ResourceCPU:    resource.MustParse("200m"),
-							v1.ResourceMemory: resource.MustParse("512Mi"),
+							v1.ResourceMemory: resource.MustParse("330Mi"),
 						},
 						Limits: v1.ResourceList{
 							v1.ResourceCPU:    resource.MustParse("200m"),
-							v1.ResourceMemory: resource.MustParse("512Mi"),
+							v1.ResourceMemory: resource.MustParse("330Mi"),
 						},
 					},
 				},
